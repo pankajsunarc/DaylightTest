cc051e163089c8fd42109dcdce27f00da6d7b2f5
LiveAction-SunArc-Dev <brij.mohan@sunarctechnologies.com>
2017-02-04 17:44:34 +0530

    Interface class by Neha ji

--- nehaisthe best.php
+++ nehaisthe best.php
@@ -1,10 +1,41 @@
-Hello , Neha Ji
-
-class myclass
+<?php
+interface MyfirstInterface
 {
-function myfunction()
-{
-echo 'hello';
+    public function setVariable($name, $var);
+    public function getHtml($template);
 }
 
+// Implement the interface
+// This will work
+class Template implements MyfirstInterface
+{
+    private $vars = array();
+  
+    public function setVariable($name, $var)
+    {
+        $this->vars[$name] = $var;
+    }
+  
+    public function getHtml($template)
+    {
+        foreach($this->vars as $name => $value) {
+            $template = str_replace('{' . $name . '}', $value, $template);
+        }
+ 
+        return $template;
+    }
 }
+
+// This will not work
+// Fatal error: Class BadTemplate contains 1 abstract methods
+// and must therefore be declared abstract (iTemplate::getHtml)
+class BadTemplate implements iTemplate
+{
+    private $vars = array();
+  
+    public function setVariable($name, $var)
+    {
+        $this->vars[$name] = $var;
+    }
+}
+?>
